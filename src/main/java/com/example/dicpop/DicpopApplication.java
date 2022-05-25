768c187e2d4453c0f518f084a77c229237317f52
Brave Lee <braveek1@gmail.com>
2022-05-25 23:21:58 +0900

    React+Springboot project init

---
+++ src/main/java/com/example/dicpop/DicpopApplication.java
@@ -0,0 +1,13 @@
+package com.example.dicpop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DicpopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DicpopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dicpop/DicpopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dicpop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DicpopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
